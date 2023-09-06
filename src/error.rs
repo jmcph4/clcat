a3f0d7b6e66e2d408dc5a6ff6fb86759e8fc8a7d
jmcph4 <jmcph4.github@gmail.com>
2023-09-06 16:38:43 +1000

    Basically p2p chat example from upstream libp2p

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,7 @@
 use std::{error::Error, fmt};
 
+use libp2p::{gossipsub::SubscriptionError, TransportError};
+
 pub const ERROR_CLIENT_INIT: u8 = 1u8;
 
 #[derive(Debug)]
@@ -40,5 +42,41 @@ impl fmt::Display for ClCatError {
 impl Error for ClCatError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         self.inner.as_ref().map(|x| x as &dyn Error)
+    }
+}
+
+impl From<TransportError<std::io::Error>> for ClCatError {
+    fn from(value: TransportError<std::io::Error>) -> Self {
+        todo!()
+    }
+}
+
+impl From<libp2p::multiaddr::Error> for ClCatError {
+    fn from(value: libp2p::multiaddr::Error) -> Self {
+        todo!()
+    }
+}
+
+impl From<SubscriptionError> for ClCatError {
+    fn from(value: SubscriptionError) -> Self {
+        todo!()
+    }
+}
+
+impl From<std::io::Error> for ClCatError {
+    fn from(value: std::io::Error) -> Self {
+        todo!()
+    }
+}
+
+impl From<&str> for ClCatError {
+    fn from(value: &str) -> Self {
+        todo!()
+    }
+}
+
+impl From<libp2p::noise::Error> for ClCatError {
+    fn from(value: libp2p::noise::Error) -> Self {
+        todo!()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
 use clap::Parser;
+use libp2p::futures::StreamExt;
+use libp2p::{
+    core::{muxing::StreamMuxerBox, transport::OrTransport, upgrade},
+    futures::future::Either,
+    gossipsub, identity, mdns, noise, quic,
+    swarm::NetworkBehaviour,
+    swarm::{SwarmBuilder, SwarmEvent},
+    tcp, yamux, PeerId, Transport,
+};
 use log::info;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+use std::time::Duration;
+use tokio::{
+    io::{self, AsyncBufReadExt},
+    select,
+};
+use tokio_stream::wrappers::LinesStream;
 
 use crate::{cli::Opts, error::ClCatError};
 
 mod cli;
 mod error;
+
+#[derive(NetworkBehaviour)]
+struct DefaultBehaviour {
+    gossipsub: gossipsub::Behaviour,
+    mdns: mdns::tokio::Behaviour,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), ClCatError> {
@@ -14,5 +37,102 @@ async fn main() -> Result<(), ClCatError> {
 
     info!("Initialised!");
 
-    Ok(())
+    // Create a random PeerId
+    let id_keys = identity::Keypair::generate_ed25519();
+    let local_peer_id = PeerId::from(id_keys.public());
+    println!("Local peer id: {local_peer_id}");
+
+    // Set up an encrypted DNS-enabled TCP Transport over the yamux protocol.
+    let tcp_transport = tcp::tokio::Transport::new(tcp::Config::default().nodelay(true))
+        .upgrade(upgrade::Version::V1Lazy)
+        .authenticate((noise::Config::new(&id_keys))?)
+        .multiplex(yamux::Config::default())
+        .timeout(std::time::Duration::from_secs(20))
+        .boxed();
+    let quic_transport = quic::tokio::Transport::new(quic::Config::new(&id_keys));
+    let transport = OrTransport::new(quic_transport, tcp_transport)
+        .map(|either_output, _| match either_output {
+            Either::Left((peer_id, muxer)) => (peer_id, StreamMuxerBox::new(muxer)),
+            Either::Right((peer_id, muxer)) => (peer_id, StreamMuxerBox::new(muxer)),
+        })
+        .boxed();
+
+    // To content-address message, we can take the hash of message and use it as an ID.
+    let message_id_fn = |message: &gossipsub::Message| {
+        let mut s = DefaultHasher::new();
+        message.data.hash(&mut s);
+        gossipsub::MessageId::from(s.finish().to_string())
+    };
+
+    // Set a custom gossipsub configuration
+    let gossipsub_config = gossipsub::ConfigBuilder::default()
+        .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
+        .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
+        .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
+        .build()?;
+
+    // build a gossipsub network behaviour
+    let mut gossipsub = gossipsub::Behaviour::new(
+        gossipsub::MessageAuthenticity::Signed(id_keys),
+        gossipsub_config,
+    )?;
+    // Create a Gossipsub topic
+    let topic = gossipsub::IdentTopic::new("test-net");
+    // subscribes to our topic
+    gossipsub.subscribe(&topic)?;
+
+    // Create a Swarm to manage peers and events
+    let mut swarm = {
+        let mdns = mdns::tokio::Behaviour::new(mdns::Config::default(), local_peer_id)?;
+        let behaviour = DefaultBehaviour { gossipsub, mdns };
+        SwarmBuilder::with_tokio_executor(transport, behaviour, local_peer_id).build()
+    };
+
+    // Read full lines from stdin
+    let mut stdin = LinesStream::new(io::BufReader::new(io::stdin()).lines()).fuse();
+
+    // Listen on all interfaces and whatever port the OS assigns
+    swarm.listen_on("/ip4/0.0.0.0/udp/0/quic-v1".parse()?)?;
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+
+    println!("Enter messages via STDIN and they will be sent to connected peers using Gossipsub");
+
+    // Kick it off
+    loop {
+        select! {
+            line = stdin.select_next_some() => {
+                if let Err(e) = swarm
+                    .behaviour_mut().gossipsub
+                    .publish(topic.clone(), line?.as_bytes()) {
+                    println!("Publish error: {e:?}");
+                }
+            },
+            event = swarm.select_next_some() => match event {
+                SwarmEvent::Behaviour(DefaultBehaviourEvent::Mdns(mdns::Event::Discovered(list))) => {
+                    for (peer_id, _multiaddr) in list {
+                        println!("mDNS discovered a new peer: {peer_id}");
+                        swarm.behaviour_mut().gossipsub.add_explicit_peer(&peer_id);
+                    }
+                },
+                SwarmEvent::Behaviour(DefaultBehaviourEvent::Mdns(mdns::Event::Expired(list))) => {
+                    for (peer_id, _multiaddr) in list {
+                        println!("mDNS discover peer has expired: {peer_id}");
+                        swarm.behaviour_mut().gossipsub.remove_explicit_peer(&peer_id);
+                    }
+                },
+                SwarmEvent::Behaviour(DefaultBehaviourEvent::Gossipsub(gossipsub::Event::Message {
+                    propagation_source: peer_id,
+                    message_id: id,
+                    message,
+                })) => println!(
+                        "Got message: '{}' with id: {id} from peer: {peer_id}",
+                        String::from_utf8_lossy(&message.data),
+                    ),
+                SwarmEvent::NewListenAddr { address, .. } => {
+                    println!("Local node is listening on {address}");
+                }
+                _ => {}
+            }
+        }
+    }
 }
